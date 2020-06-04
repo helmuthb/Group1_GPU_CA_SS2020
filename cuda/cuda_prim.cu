489619a98c0d60f34bc9c2388b851102f751a2d9
Christian Kastner <ckk@kvr.at>
2020-06-04 21:56:14 +0200

    Drop redundant conditional
    The case
    
        v == 1  -> outbound_vertices[v-1].x
    
    is a special case of
    
        v > 0   -> outbound_vertices[v - 1].y + outbound_vertices[v-1].x
    
    because in the latter case, when v == 1, outbound_vertices[v-1].y == 0.
    So we only need to cover v == 0, and v > 0.

--- cuda/cuda_prim.cu
+++ cuda/cuda_prim.cu
@@ -13,7 +13,7 @@ void cudaSetup(const Graph& g, uint2 *&inbound_vertices, uint2 *&outbound_vertic
         std::vector<EdgeTarget> neighbors;
         g.neighbors(v, neighbors);
         outbound_vertices[v].x = neighbors.size();
-        outbound_vertices[v].y = v == 0 ? 0 : v == 1 ? outbound_vertices[v-1].x : outbound_vertices[v - 1].y + outbound_vertices[v-1].x;
+        outbound_vertices[v].y = v == 0 ? 0 : outbound_vertices[v - 1].y + outbound_vertices[v-1].x;
         for (auto nb = neighbors.begin(); nb < neighbors.end(); ++nb) {
             inbound_vertices[pos].x = nb->vertex_to;
             inbound_vertices[pos++].y = nb->weight;
